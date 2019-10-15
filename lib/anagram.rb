e8c90f951faed74527b220a774f78746c8886fe0
Maynard Hartman <mjhartman@brighthouse.com>
2019-10-15 15:38:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word) 
+    @word = word
+  end
+  
+  def match(words)
+    words.each { |x|
+     if @word.sort == x.split("").sort then
+       puts "#{@word}"
+    end
+    }
+  end
+end
